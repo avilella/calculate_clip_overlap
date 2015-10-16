fccd7ae3bd6ce73a88417940c56d9f5b14965f07
Albert Vilella <avilella@gmail.com>
2015-10-16 13:50:10 +0100

    obs option

--- calculate_clip_overlap.pl
+++ calculate_clip_overlap.pl
@@ -12,12 +12,14 @@ my $iter = 100000;
 my $genome_size = 3000000000;
 my $range = "75:100:125:150:250";
 my $tag = 'clip_overlap';
+my $obs = '2x75';
 GetOptions(
 	   'i|input|inputfile:s' => \$inputfile,
 	   'iter:s'              => \$iter,
            'genome_size:s'       => \$genome_size,
 	   'range:s'             => \$range,
 	   'tag:s'               => \$tag,
+	   'obs|observed:s'      => \$obs,
            'debug'    => \$debug,
            'verbose'  => \$verbose,
 	   'simulate' => \$simulate,
@@ -73,13 +75,14 @@ sub summary {
   print $outh "#avg_tlen=$this_avg_tlen\n";
   print $outh "SEQ\tread_PE\tscov\ttotal_nuc\tmapped_nuc\tlost_nuc\teffic_pc\n";
   foreach my $cycles (@ranges) {
-    print $outh "2x$cycles" . "\t" .
-      $count . "\t" .
+    my $str = $count . "\t" .
       sprintf("%.05f",($stats->{total}{$cycles})/$genome_size) . "\t" .
       $stats->{total}{$cycles} . "\t" .
       $stats->{mapped_nuc}{$cycles} . "\t" .
       $stats->{lost_nuc}{$cycles} . "\t" .
       sprintf("%.02f",100-((100*($stats->{lost_nuc}{$cycles}))/$stats->{total}{$cycles})) . "\n";
+    print $outh "obs" . "\t" . $str if ($obs eq "2x$cycles");
+    print $outh "2x$cycles" . "\t" . $str;
   }
 
   return;
